bf6c89842a43dfaa53344a94a63ad16eb52910ee
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-10 06:31:28 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist 
+  
+  attr_accessor :name
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.each {|song| song.artist = self}
+  end
+  
+  def add_song(song)
+    song.artist = self
+    song = Song.new(song)
+  end  
+  
+end
