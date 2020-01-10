3a78d9074019acb8cde69597311d482a366bbb20
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-10 06:39:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end  
   
   def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.
+    self.find(name) ? self.find(name) : self.new(name)
   end
   
 end
